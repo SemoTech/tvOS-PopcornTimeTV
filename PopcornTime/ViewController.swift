772ac38023351804810795e2530514776dc006ad
Pedro Piñera <pepibumur@gmail.com>
2015-12-24 17:52:34 +0100

    Created project

---
+++ PopcornTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PopcornTime
+//
+//  Created by Pedro Pinera Buendia on 24/12/15.
+//  Copyright © 2015 com.PPinera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

d9814c86eb0b2c2d51a3ce3dbab0a49af95433db
Pedro Piñera <pepibumur@gmail.com>
2015-12-25 10:26:46 +0100

    Added VLCKit as a submodule

--- Pods/Target Support Files/Pods-PopcornTime/Pods-PopcornTime-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PopcornTime : NSObject
-@end
-@implementation PodsDummy_Pods_PopcornTime
-@end
--- Pods/Target Support Files/Pods-PopcornTime/Pods-PopcornTime-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_PopcornTimeVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_PopcornTimeVersionString[];
-

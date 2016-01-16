4356b015a8755718fe8e1c0388f65caefeb368d1
Pedro Piñera <pepibumur@gmail.com>
2016-01-16 23:14:31 +0100

    Forced top shelf synchronisation when the app is opened.

--- PopcornTime/Source/App/AppDelegate.swift
+++ PopcornTime/Source/App/AppDelegate.swift
@@ -1,16 +1,29 @@
 import UIKit
+import TVServices
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    // MARK: - Attributes
+    
     var window: UIWindow?
 
+    
+    // MARK: - Lifecycle
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
         self.window?.rootViewController = PlayerView()
         self.window?.makeKeyAndVisible()
+        forceTopShelfUpdate()
         return true
+    }
+    
+    
+    // MARK: - Private
+    
+    private func forceTopShelfUpdate() {
+        NSNotificationCenter().postNotificationName(TVTopShelfItemsDidChangeNotification, object: nil)
     }
 }
 
